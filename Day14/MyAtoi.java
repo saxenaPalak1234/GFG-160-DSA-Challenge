206baa9a46540e100c2c4364e7a42f3f687b9165
Palak Saxena <128035394+saxenaPalak1234@users.noreply.github.com>
2025-04-10 23:09:09 +0530

    Create MyAtoi.java

---
+++ Day14/MyAtoi.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int myAtoi(String s) {
+        int idx = 0, sign = 1;
+        long res = 0;
+
+        while (idx < s.length() && s.charAt(idx) == ' ') idx++;
+
+        if (idx < s.length() && (s.charAt(idx) == '-' || s.charAt(idx) == '+')) {
+            sign = (s.charAt(idx++) == '-') ? -1 : 1;
+        }
+
+        while (idx < s.length() && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
+            res = res * 10 + (s.charAt(idx++) - '0');
+
+            if (res * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (res * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        }
+
+        return (int)(res * sign);
+    }
+}
